a6123744aa1329e4f8408f6407113587cf0e4107
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-22 23:19:26 +0400

    a

--- skr4.sh
+++ skr4.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 for pid in $(ps -ax -o pid)
 do
-PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}')`
-Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}')`
-Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}')`
+PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}'`
+Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}'`
+Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}'`
 if [[ -n $Switch && -n $Runtime ]]
 then
 Art=$(bc <<< "scale=5; $Runtime/$Switch")
