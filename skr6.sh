7c1d08251922e45d5b5bfb60feb21a7dd86e40e9
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-23 00:53:52 +0400

    b

--- skr6.sh
+++ skr6.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 MaxSize=0
 Pid_of_MaxSize=0
-for pid in $(ps -a -u root o pid)
+for pid in $(ps -ax -o pid)
 do
 Size=`grep -s "VmSize" "/proc/"$pid"/status" | awk '{print $2}'`
 if [[ $Size -gt $MaxSize ]]
@@ -10,5 +10,7 @@ MaxSize=$Size
 Pid_of_MaxSize=$pid
 fi
 done
-top -p $Pid_of_MaxSize -b -n 1 > task6.txt
+top -p $Pid_of_MaxSize -b -n 1 > buffer.txt
 vmSize=$(cat task6.txt | grep $Pid_of_MaxSize | awk '{print $5 }')
+echo "Pid:$Pid_of_MaxSize MEM in /proc $MaxSize MEM in top $vmSize" > t6.txt
+rm buffer.txt
