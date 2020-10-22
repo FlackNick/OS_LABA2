dd520cfa59bea487ba58966eff8ab1ef0f1da34b
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-22 22:37:09 +0400

    skr5 is ready

---
+++ skr4.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+for pid in $(ps -ax -o pid)
+do
+PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}')`
+Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}')`
+Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}')`
+if [[ -n $Switch && -n $Runtime ]]
+then
+Art=$(bc <<< "scale=5; $Runtime/$Switch")
+echo "ProcessID=$pid : Parent_ProcessID=$PPid : Average_Running_Time=$Art"
+fi
+done | sort -n -t = -k 3 > t4.txt
