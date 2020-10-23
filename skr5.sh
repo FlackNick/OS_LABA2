877a37a047225cc272e51e80985c1ddd6da84f73
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-23 15:05:37 +0400

    All is ready

--- skr5.sh
+++ skr5.sh
@@ -1,29 +1,29 @@
 #! /usr/bin/env bash
-prevPPid=-1
 sum=0
 counter=0
+prev_ppid=-999
 
 while read line
 do
     runtime=`echo $line | cut -d ":" -f 3 | cut -d "=" -f 2`
     PPid=`echo $line | cut -d ":" -f 2 | cut -d "=" -f 2`
-    if [[ $prevPPid -eq -1 ]]
+    if [[ $prev_ppid -eq -999 ]]
     then
         echo $line
-        prevPPid=$PPid
+        prev_ppid=$PPid
         sum=$runtime
         counter=1
         continue
     fi
-    if [[ $PPid -eq $prevPPid ]]
+    if [[ $PPid -eq $prev_ppid ]]
     then
         echo $line
         sum=`bc <<< "scale=3; $runtime + $sum"`
         count=$(( count + 1 ))
     else
-        echo "Average_Sleeping_children_of_ParentID=$prevPPid is" `bc <<< "scale=3; $sum / $count"`
+        echo "Average_Sleeping_children_of_ParentID=$prev_ppid is" `bc <<< "scale=3; $sum / $count"`
         echo $line
-        prevPPid=$PPid
+        prev_ppid=$PPid
         sum=$runtime
         counter=1
     fi
