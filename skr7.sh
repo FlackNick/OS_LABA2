f12e0db7787b18c8fb69b09013d7ce9d557c5cc9
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-23 12:16:01 +0400

    all is ready

--- skr7.sh
+++ skr7.sh
@@ -1,24 +1,24 @@
 #! /usr/bin/env bash
-for i in $(ps -a -u root o pid,command | awk '{print $1 ":" $2}')
+for line in $(ps -ax -o pid,command | awk '{print $1 ":" $2}')
 do
-pid=$(echo $i | cut -d ":" -f 1)
-command=$(echo $i | cut -d ":" -f 2)
-byte=$(grep -s "read_bytes" /proc/$pid/io | cut -d ":" -f 2)
-if [[ -n $byte ]]
+pid=$(echo $line | cut -d ":" -f 1)
+command=$(echo $line | cut -d ":" -f 2)
+mem=$(grep -s "read_bytes" /proc/$pid/io | cut -d ":" -f 2)
+if [[ -n $mem ]]
 then
-echo $pid $command $byte
+echo $pid $command $mem
 fi
-done > temp7.txt
+done > t7.txt
 sleep 1m
-for i in $(ps -a -u root o pid,command | awk '{print $1 ":" $2}')
+for line in $(ps -ax -o pid,command | awk '{print $1 ":" $2}')
 do
-Pid=$(echo $i | cut -d ":" -f 1)
-Command=$(echo $i | cut -d ":" -f 2)
-now_byte=$(grep -s "read_bytes" /proc/$Pid/io | cut -d ":" -f 2)
-past_byte=$(grep $Pid temp7.txt | awk '{print $3}')
-if [[ -n $now_byte && -n $past_byte ]]
-count_bytes=$(($now_byte - $past_byte))
-echo "Pid: $Pid Command: $Command Bytes:$count_bytes"
+Pid=$(echo $line | cut -d ":" -f 1)
+Command=$(echo $line | cut -d ":" -f 2)
+new_mem=$(grep -s "read_bytes" /proc/$Pid/io | cut -d ":" -f 2)
+last_mem=$(grep $Pid buffer.txt | awk '{print $3}')
+if [[ -n $new_mem && -n $last_mem ]]
+answer=$(($new_mem - $last_mem))
+echo "Pid: $Pid Command: $Command Bytes:$answer"
 fi
-done | sort -n -t ':' -k 3 | head -3 > task7.txt
-rm temp7.txt
+done | sort -n -t ':' -k 3 | head -3 > t7.txt
+rm buffer.txt
--- testf.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-function funs(){
-    echo "ProcessID = 235 : Parent_ProcessID = 234 : Avegage_Time = 456"
-}
-as=$(bc <<< "scale=1;8646893.8649324 + 65325795.2345667")
-echo $as
