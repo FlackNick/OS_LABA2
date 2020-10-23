84174784abb5c0aad54310f7a6d653f0bd090384
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-23 14:49:32 +0400

    All is ready

--- skr7.sh
+++ skr7.sh
@@ -3,7 +3,7 @@ for line in `ps -ax -o pid,command | awk '{print $1 "," $2}'`
 do
     pid=`echo $line | cut -d "," -f 1`
     command=`echo $line | cut -d "," -f 2`
-    mem=`grep -s "read_bytes" /proc/$pid/io | cut -d ":" -f 2`
+    mem=`grep -s "read_bytes" "/proc/$pid/io" | cut -d ":" -f 2`
     if [[ -n $mem ]]
     then
         echo $pid $command $mem
@@ -14,7 +14,7 @@ for line in `ps -ax -o pid,command | awk '{print $1 "," $2}'`
 do
     Pid=`echo $line | cut -d "," -f 1`
     Command=`echo $line | cut -d "," -f 2`
-    new_mem=`grep -s "read_bytes" /proc/$Pid/io | cut -d ":" -f 2`
+    new_mem=`grep -s "read_bytes" "/proc/$Pid/io" | cut -d ":" -f 2`
     last_mem=`grep $Pid buffer.txt | awk '{print $3}'`
     if [[ -n $new_mem && -n $last_mem ]]
         answer=$(($new_mem - $last_mem))
