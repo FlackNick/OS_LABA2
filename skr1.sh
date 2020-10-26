fe7efed48309b35eccb2bfffc94939bda37564e2
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-10-26 12:57:14 +0400

    All is ready

--- skr1.sh
+++ skr1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps -axu | sed 1d | wc -l > t1.txt
-ps -axu | sed 1d | awk '{print $2 ": " $11}' >> text1.txt
+ps -u | sed 1d | wc -l > t1.txt
+ps -u | sed 1d | awk '{print $2 ": " $11}' >> text1.txt
